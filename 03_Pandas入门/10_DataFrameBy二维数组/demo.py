196f93d7e093c7cd564f8e21cad30204dac6bc08
Jacob-xyb <949782197@qq.com>
2020-11-26 16:51:26 +0800

    df二维数组的输入及输出形式

--- 03_Pandas入门/10_DataFrameBy二维数组/demo.py
+++ 03_Pandas入门/10_DataFrameBy二维数组/demo.py
@@ -6,4 +6,11 @@ data = [[110, 105, 99], [105, 88, 115], [109, 120, 130]]
 columns = ['语文', '数学', '英语']
 df = pd.DataFrame(data=data, columns=columns)
 print(df)
+print(df.transpose())
 
+df_1 = pd.DataFrame(data=data)
+print(df_1)
+
+df_2 = pd.DataFrame(data)
+print(df_2)
+# df, df_1, df_2 结果形式是一样的 是将列表按照矩阵的形式输入dataframe
